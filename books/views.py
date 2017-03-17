f271610b24cc5b757cf94cdfb3dcc21005abc804
NT <nt@google.com>
2017-03-17 15:18:20 +0530

    fifth

--- books/views.py
+++ books/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpResponse
+from books.models import Book
 # Create your views here.
 
 def display_met(request):
@@ -14,8 +15,15 @@ def search_form(request):
     return render(request, 'search_form.html')
 
 def search(request):
-    if 'q' in request.GET:
-        message = 'You searched for: %s' % request.GET['q']
+    error = False
+    if 'q' in request.GET and request.GET['q']:
+        q = request.GET['q']
+        if not q:
+            error = True
+        else:
+            books = Book.objects.filter(title__icontains=q)
+            return render(request, 'search_results.html', {'books': books, 'query': q})
+#        message = 'You searched for: %s' % request.GET['q']
     else:
-        message = 'You submitted an empty string'
+        return render(request, 'search_form.html', {'error': error})
     return HttpResponse(message)
